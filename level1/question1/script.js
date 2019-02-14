f39ab924f37ff4106abbac3c47afb1ed65072305
Simon Augustin Vonheim <fed@bdda.no>
2019-02-14 03:26:25 +0100

    1

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,13 @@
-//Use RegEx to validate form
+document.getElementById("submit").addEventListener("click", function(){
+    var inputFields  = document.getElementsByTagName("input");
+    
+    for(var i = 0; i < inputFields.length; i++){ 
+        var theInput = inputFields[i].value;
+        console.log(theInput);
+        var thePattern = new RegExp(inputFields[i].pattern);
+        var theTest = thePattern.test(theInput);
+        if (theTest === false){
+            console.log(inputFields[i].name + " is not valid");
+        }
+    }
+})
