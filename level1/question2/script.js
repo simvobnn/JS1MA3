0057b67d6c5c8a5adbeef5052bbbf3169baca7ea
Simon Augustin Vonheim <fed@bdda.no>
2019-02-16 14:45:12 +0100

    Assignment complete

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,26 @@
-//Console.log out elements in JSON file
+var jsonObject = {
+	"video": [{
+		"id": 12312412312,
+		"name" : "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+};
+
+console.log(jsonObject.video[0].id);
+console.log(jsonObject.video[0].name);
+console.log(jsonObject.video[0].url);
+console.log(jsonObject.video[0].author);
+for (var i=0;i < jsonObject.video[0].author.data.length;i++){
+    console.log(jsonObject.video[0].author.data[i]);
+}
+
+    
+
+ 
