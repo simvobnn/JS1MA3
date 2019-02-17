a8ea5c3ab6637e5fb54f2f17d8838dac324fed96
Simon Augustin Vonheim <fed@bdda.no>
2019-02-17 21:56:28 +0100

    Assignment complete

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,26 +1,28 @@
 var jsonObject = {
 	"video": [{
-		"id": 12312412312,
-		"name" : "Ecuaciones Diferenciales",
-		"url": "/video/math/edo/12312412312",
-		"author": {
+		"id": 12312412312,	//logged
+		"name" : "Ecuaciones Diferenciales",	//logged
+		"url": "/video/math/edo/12312412312",	//logged
+		"author": {		//logged, but excluded
 			"data": [{
-				"name_author": "Alejandro Morales",
-				"url": "/author/alejandro-morales",
-				"type": "master"
+				"name_author": "Alejandro Morales",		//logged
+				"url": "/author/alejandro-morales",		//logged
+				"type": "master"	//logged
 			}]
 		}
 	}]
 };
-
+var jsonObjectArray = (Object.values(jsonObject.video[0]));
+for (var i=0, jOALength = jsonObjectArray.length;i < jOALength -1;i++){
+	console.log(jsonObjectArray[i]);
+}
+/*
 console.log(jsonObject.video[0].id);
 console.log(jsonObject.video[0].name);
 console.log(jsonObject.video[0].url);
 console.log(jsonObject.video[0].author);
-for (var i=0;i < jsonObject.video[0].author.data.length;i++){
-    console.log(jsonObject.video[0].author.data[i]);
+*/
+var dataArray = (Object.values(jsonObject.video[0].author.data[0]));
+for (var i=0, dALength = dataArray.length;i < dALength;i++){
+	console.log(dataArray[i]);
 }
-
-    
-
- 
